32a48c8e6279ec17d99990d65ef268fb8f1fc266
Łukasz Langa <lukasz@langa.pl>
2021-05-10 20:17:24 +0200

    Add a basic AD envelope

--- fm/__init__.py
+++ fm/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import *
 
 from array import array
+from dataclasses import dataclass
 import math
 import miniaudio
 import time
@@ -9,6 +10,9 @@ import time
 __version__ = "0.1.0"
 
 INT16_MAX = 2 ** 15 - 1
+
+if TYPE_CHECKING:
+    Audio = Generator[array[int], int, None]
 
 
 def sine_array(sample_count: int) -> array[int]:
@@ -19,7 +23,48 @@ def sine_array(sample_count: int) -> array[int]:
     return array("h", numbers)
 
 
-def endless_sine(sample_count: int) -> Generator[array[int], int, None]:
+@dataclass
+class Envelope:
+    # all units in samples
+    attack: int
+    decay: int
+    samples_advanced: int = 0
+    current_value: float = 0.0
+
+    def advance(self) -> float:
+        envelope = self.current_value
+        a = self.attack
+        d = self.decay
+        advanced = self.samples_advanced
+
+        if advanced == -1:
+            return 0.0
+
+        if advanced <= a:
+            envelope = 1.0 if a == 0 else advanced / a
+        elif d > 0 and advanced - a <= d:
+            envelope = 1.0 - (advanced - a) / d
+        else:
+            envelope = 0.0
+            advanced = -2
+        self.samples_advanced = advanced + 1
+        self.current_value = envelope
+        return envelope
+
+
+def envelop(audio: Audio, envelope: Envelope) -> Audio:
+    result = next(audio)
+    want_frames = yield result
+
+    out_buffer = array("h", [0] * want_frames)
+    while True:
+        mono_buffer = audio.send(want_frames)
+        for i in range(want_frames):
+            out_buffer[i] = int(envelope.advance() * mono_buffer[i])
+        want_frames = yield out_buffer[:want_frames]
+
+
+def endless_sine(sample_count: int) -> Audio:
     sine = sine_array(sample_count)
     result = array("h")
     want_frames = yield result
@@ -55,7 +100,7 @@ def get_miniaudio_playback_device(name: str) -> miniaudio.PlaybackDevice:
 
 
 def main() -> None:
-    stream = endless_sine(100)
+    stream = envelop(endless_sine(100), Envelope(attack=410, decay=44100))
     next(stream)
     with get_miniaudio_playback_device("BlackHole 16ch") as dev:
         dev.start(stream)
